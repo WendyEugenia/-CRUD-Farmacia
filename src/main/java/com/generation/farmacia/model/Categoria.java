b63b1375fd1036923dd7dce7b72aba2ebe6ae41d
WendyEugenia <wendy.t.n@hotmail.com>
2024-01-04 12:27:47 -0300

    add model, repository e controller

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,57 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome categoria é obrigatorio!")
+	@Size(min = 3, max = 100, message = "O atributo nome categoria deve conter no mínimo 02 e no máximo 100 caracteres")
+	@Column(length = 100)
+	private String nome;
+	
+	@NotBlank(message = "A categoria deve ter uma descrição!")
+	@Size(min = 10, max = 250, message = "A descrição deve ter entre 5 á 250 caracteres.")
+	@Column(length = 250)
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	
+
+}
